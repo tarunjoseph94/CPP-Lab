e9a7ab7936f4c587403a48593cc4703260d8b952
Tarun Joseph <tarunjoseph94@gmail.com>
2018-04-19 13:02:17 +0530

    Add files via upload

--- friendnoverloading.cpp
+++ friendnoverloading.cpp
@@ -1,20 +1,25 @@
-#include <iostream>
+# include <iostream>
 using namespace std;
 class Count
 {
 private:
-	int c=0;
+	 int c;
 public:
-	
-	friend void operator ++ (Count a);
+	Count(int a)
+	{
+	c=a;
+	}
+	friend Count operator ++(Count);
 	// friend void operator ++ (int,Count a);
 	
 };
- void operator ++ (Count a)
+ Count operator ++(Count a)
  {
- 	a.c++;
+  	a.c++;
  	cout<<a.c<<"\n";
+ 	return a.c;
  }
+
  //  void operator ++ (int,Count a)
  // {
  // 	a.c++;
@@ -22,7 +27,38 @@ public:
  // }
  int main()
  {
- 	Count c1;
- 	 ++c1	;
- 	 ++ c1;
+ 	Count c1(0);
+ 	 c1=++c1;
+ 	 ++c1;
  }
+
+
+
+//  using namespace std; 
+//  class myClass
+//  { 
+//  	private: 
+//  		int a;
+//   	public: 
+//   	myClass(int x)
+//   	{ 
+//   		a=x; 
+//   	} 
+//   	bool friend operator>(myClass,myClass); 
+//   	void friend operator++(myClass); }; 
+//   	bool operator>(myClass b1, myClass b2)
+//   	{ 
+//   		return b1.a<b2.a; 
+//   	} 
+//   	void operator++(myClass b1)
+//   	{ 
+//   		b1.a++; 
+//   		cout<<"Value of a:"<<b1.a<<"\n";
+// 	}
+// int main()
+// { 
+// 	myClass a(10),b(11); 
+// 	++a; 
+// 	a++; 
+// 	cout<<"A>B"<<(a>b)<<endl; 
+// } 
